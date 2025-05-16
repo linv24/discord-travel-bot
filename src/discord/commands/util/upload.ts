318e4f1f817d8ddb7a2de79b9d22d60b02ff9fe3
Vincent Lin <linv@mit.edu>
2025-05-16 00:07:35 -0400

    add command handler

--- src/discord/commands/util/upload.ts
+++ src/discord/commands/util/upload.ts
@@ -1,10 +1,10 @@
-import { SlashCommandBuilder, Interaction } from "discord.js";
+import { SlashCommandBuilder, ChatInputCommandInteraction } from "discord.js";
 
-module.exports = {
+export default {
     data: new SlashCommandBuilder()
         .setName("upload")
         .setDescription("Upload a screenshot of flight itinerary."),
-    async execute(interaction) {
+    async execute(interaction: ChatInputCommandInteraction) {
         await interaction.reply("WIP: upload screenshot");
     },
 };
--- src/discord/index.ts
+++ src/discord/index.ts
@@ -1,12 +1,57 @@
 import fs from "node:fs";
 import path from "node:path";
-import { Client, Collection, Events, GatewayIntentBits } from "discord.js";
-import "./types/discrd";
+import { ChatInputCommandInteraction, Client, Collection, Events, GatewayIntentBits, Message, MessageFlags } from "discord.js";
+import "../types/discord";
+import { Command } from "../types/Command";
 import "dotenv/config";
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
+
+const foldersPath = path.join(__dirname, "commands");
+const commandFolders = fs.readdirSync(foldersPath)
+
+for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith(".js"));
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = (await import(filePath)).default;
+        // Set a new item in the Collection with the key as the command name and the value as the exported module
+        if ("data" in command && "execute" in command) {
+            client.commands.set(command.data.name, command);
+        } else {
+            console.log(`[WARNING] The command at ${filePath} is missing required "data" or "execute property.`);
+        }
+    }
+}
+
+client.on(Events.InteractionCreate, async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = interaction.client.commands.get(interaction.commandName);
+    if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found`);
+        return;
+    }
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        if (interaction.replied || interaction.deferred) {
+            await interaction.followUp({
+                content: "There was an error while executing this command!",
+                flags: MessageFlags.Ephemeral
+            });
+        } else {
+            await interaction.reply({
+                content: "There was an error while executing this command!",
+                flags: MessageFlags.Ephemeral
+            });
+        }
+    }
+});
 
 // When the client is ready, run this code (only once)
 client.once(Events.ClientReady, readyClient => {
