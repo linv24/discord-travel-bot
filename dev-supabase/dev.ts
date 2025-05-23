b8ed6eb113e16968251f6687487f75de9a9e1fcf
Vincent Lin <vincentlin246@gmail.com>
2025-05-23 13:00:19 -0400

    implement inserting trip json into supabase

--- dev-supabase/dev.ts
+++ dev-supabase/dev.ts
@@ -16,7 +16,7 @@ const supabaseKey = process.env.SUPABASE_KEY
 // TODO: some fields can be null, enforce this in interface
 interface TripJSON {
   name: string
-  notes: string
+  description: string
   start_timestamp: string
   end_timestamp: string
   journeys: {
@@ -26,8 +26,8 @@ interface TripJSON {
     legs: {
       flight_number: string
       airline: string
-      departure_airport: string
-      arrival_airport: string
+      departure_airport_code: string
+      arrival_airport_code: string
       departure_timestamp: string
       arrival_timestamp: string
     }[]
@@ -43,7 +43,7 @@ async function main(discord_id: string, trip: TripJSON) {
 
     const supabase = createClient(supabaseUrl, supabaseKey);
 
-    const userId = utils.getUserId(supabase, discord_id)
+    const userId = await utils.getUserId(supabase, discord_id)
 
     // insert trip
     const tripId = uuidv4()
@@ -53,17 +53,46 @@ async function main(discord_id: string, trip: TripJSON) {
             id: tripId,
             user_id: userId,
             name: trip.name,
-            notes: trip.notes,
+            description: trip.description,
             start_timestamp: trip.start_timestamp,
             end_timestamp: trip.end_timestamp,
         })
     if (tripError) throw tripError;
 
     // insert journey
+    for (const journey of trip.journeys) {
+        const journeyId = uuidv4()
+        const { error: journeyError } = await supabase
+            .from("journeys")
+            .insert({
+                id: journeyId,
+                trip_id: tripId,
+                confirmation: journey.confirmation,
+                departure_timestamp: journey.departure_timestamp,
+                arrival_timestamp: journey.arrival_timestamp,
+            })
+        if (journeyError) throw journeyError;
 
-    // insert legs
+        // insert legs
+        const legs = journey.legs.map((leg) => ({
+            id: uuidv4(),
+            journey_id: journeyId,
+            flight_number: leg.flight_number,
+            airline: leg.airline,
+            departure_airport_code: leg.departure_airport_code,
+            departure_timestamp: leg.departure_timestamp,
+            arrival_airport_code: leg.arrival_airport_code,
+            arrival_timestamp: leg.arrival_timestamp,
+        }));
+        const { error: legsError } = await supabase
+            .from("legs")
+            .insert(legs)
+        if (legsError) throw legsError;
+    }
+
+    return { success: true, tripId };
 }
 
 const discord_id = "238837641473163264"
 import trip_data from "./sample_data.json";
-main(discord_id, trip_data)
+await main(discord_id, trip_data)
